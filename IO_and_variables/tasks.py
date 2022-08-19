f557b6cd2ac0129dfbaa172c2372f12c76349103
gvarys <johnnyzaev@gmail.com>
2022-08-19 16:25:55 +0300

    IO tasks done!

---
+++ IO_and_variables/tasks.py
@@ -0,0 +1,14 @@
+print("Здравствуй, мир!")
+print("4", "8", "15", "16", "23", "42")
+print(4)
+print(8)
+print(15)
+print(16)
+print(23)
+print(42)
+i = 1
+while (i < 8):
+    print("*" * i)
+    i += 1
+name = input()
+print("Привет,", name)
