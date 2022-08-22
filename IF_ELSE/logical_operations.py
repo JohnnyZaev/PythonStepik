5fabfeef42083b9cbc59dbed5871844a6ded3956
gvarys <johnnyzaev@gmail.com>
2022-08-22 20:41:54 +0300

    4 tasks from operations done!

--- IF_ELSE/logical_operations.py
+++ IF_ELSE/logical_operations.py
@@ -8,3 +8,15 @@ if (n <= -3 or n >= 7):
     print("Принадлежит")
 else:
     print("Не принадлежит")
+
+x = int(input())
+if (-30 < x <= -2 or 7 < x <= 25):
+    print("Принадлежит")
+else:
+    print("Не принадлежит")
+
+n = int(input())
+if (999 < n < 10000) and (n % 7 == 0 or n % 17 == 0):
+    print("YES")
+else:
+    print("NO")
