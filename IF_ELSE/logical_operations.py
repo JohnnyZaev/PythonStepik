1c973be9c14365be3e267b240347ea001a790a67
gvarys <johnnyzaev@gmail.com>
2022-08-22 20:32:09 +0300

    2 tasks done from operations

---
+++ IF_ELSE/logical_operations.py
@@ -0,0 +1,10 @@
+if (-1 < int(input()) < 17):
+    print("Принадлежит")
+else:
+    print("Не принадлежит")
+
+n = int(input())
+if (n <= -3 or n >= 7):
+    print("Принадлежит")
+else:
+    print("Не принадлежит")
