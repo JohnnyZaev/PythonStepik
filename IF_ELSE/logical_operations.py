e1dcd30e4a8b69ea3b386a9b9990f3f190c0d54c
gvarys <johnnyzaev@gmail.com>
2022-08-22 20:48:11 +0300

    6 tasks from operations done!

--- IF_ELSE/logical_operations.py
+++ IF_ELSE/logical_operations.py
@@ -20,3 +20,15 @@ if (999 < n < 10000) and (n % 7 == 0 or n % 17 == 0):
     print("YES")
 else:
     print("NO")
+
+a, b, c = int(input()), int(input()), int(input())
+if (a < b + c and b < c + a and c < a + b):
+    print("YES")
+else:
+    print("NO")
+
+year = int(input())
+if ((year % 4 == 0 and year % 100 != 0) or year % 400 == 0):
+    print("YES")
+else:
+    print("NO")
