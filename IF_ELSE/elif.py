6197e3f3cbf2d4571d466917edf2039efccf5af4
gvarys <johnnyzaev@gmail.com>
2022-08-22 22:05:22 +0300

    7 tasks from elif done

--- IF_ELSE/elif.py
+++ IF_ELSE/elif.py
@@ -41,3 +41,39 @@ elif weight < 64:
     print("Первый полусредний вес")
 else:
     print("Полусредний вес")
+
+a, b, c = int(input()), int(input()), input()
+if c == "+":
+    print(a + b)
+elif c == "-":
+    print(a - b)
+elif c == "*":
+    print(a * b)
+elif c == "/":
+    if b == 0:
+        print("На ноль делить нельзя!")
+    else:
+        print(a / b)
+else:
+    print("Неверная операция")
+
+a, b = input(), input()
+if ((a == "красный" or a == "синий" or a == "желтый") and (b == "красный" or b == "синий" or b == "желтый")):
+    result = 0
+    if (a == b):
+        print(a)
+    else:
+        if (a == "красный" or b == "красный"):
+            result += 1
+        if (a == "синий" or b == "синий"):
+            result += 2
+        if (a == "желтый" or b == "желтый"):
+            result += 3
+        if (result == 3):
+            print("фиолетовый")
+        if (result == 4):
+            print("оранжевый")
+        if (result == 5):
+            print("зеленый")
+else:
+    print("ошибка цвета")
