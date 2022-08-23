46693b01a5f234ddf208b04373ad01f655a73719
gvarys <johnnyzaev@gmail.com>
2022-08-23 12:08:04 +0300

    5 tasks from nombers is done!

--- Data_types/numbers.py
+++ Data_types/numbers.py
@@ -1,2 +1,24 @@
 a, b = float(input()), float(input())
 print(0.5 * a * b)
+
+s, v1, v2 = float(input()), float(input()), float(input())
+v = v1 + v2
+print(s / v)
+
+f = float(input())
+print(5 / 9 * (f - 32))
+
+x = float(input())
+if (x == 0):
+    print("Обратного числа не существует")
+else:
+    print(1 / x)
+
+n = int(input())
+result = 0
+for i in range(n):
+    if (i < 2):
+        result += 10.5
+    else:
+        result += 4
+print(result)
