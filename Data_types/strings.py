666ac3a23c052e88e0a9442070419d77550070cd
gvarys <johnnyzaev@gmail.com>
2022-09-01 17:43:47 +0300

    4th task from strings

--- Data_types/strings.py
+++ Data_types/strings.py
@@ -7,3 +7,18 @@ print("You just delved into Python")
 
 name = input()
 print("Футбольная команда", name, "имеет длину", len(name), "символов")
+
+town1, town2, town3 = input(), input(), input()
+len1, len2, len3 = len(town1), len(town2), len(town3)
+if (min(len1, len2, len3) == len1):
+    print(town1)
+elif (min(len1, len2, len3) == len2):
+    print(town2)
+else:
+    print(town3)
+if (max(len1, len2, len3) == len1):
+    print(town1)
+elif (max(len1, len2, len3) == len2):
+    print(town2)
+else:
+    print(town3)
