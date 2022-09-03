80b7803a63b45f4dc51642a527d34930e995d88a
gvarys <johnnyzaev@gmail.com>
2022-09-03 13:58:00 +0300

    3rd task from math

--- Data_types/math.py
+++ Data_types/math.py
@@ -5,3 +5,7 @@ print(math.dist((x1, y1), (x2, y2)))
 import math
 R = float(input())
 print(math.pi * R ** 2, math.pi * 2 * R, sep='\n')
+
+import math
+a, b = float(input()), float(input())
+print((a + b) / 2, math.sqrt(a * b), 2 * a * b / (a + b), math.sqrt((a ** 2 + b ** 2) / 2), sep = '\n')
