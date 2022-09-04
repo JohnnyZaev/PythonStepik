5ce6ddf038b1de1231dcaaa417bfeeb30571f79a
gvarys <johnnyzaev@gmail.com>
2022-09-04 18:33:28 +0300

    4th task from math

--- Data_types/math.py
+++ Data_types/math.py
@@ -2,10 +2,11 @@ import math
 x1, y1, x2, y2 = float(input()), float(input()), float(input()), float(input())
 print(math.dist((x1, y1), (x2, y2)))
 
-import math
 R = float(input())
 print(math.pi * R ** 2, math.pi * 2 * R, sep='\n')
 
-import math
 a, b = float(input()), float(input())
 print((a + b) / 2, math.sqrt(a * b), 2 * a * b / (a + b), math.sqrt((a ** 2 + b ** 2) / 2), sep = '\n')
+
+x = float(input())
+print(math.sin(math.radians(x)) + math.cos(math.radians(x)) + math.tan(math.radians(x)) ** 2)
