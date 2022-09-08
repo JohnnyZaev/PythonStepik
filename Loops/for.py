e447c298043fdc200c1e3c6a3159c574f7f83ec0
gvarys <johnnyzaev@gmail.com>
2022-09-08 15:00:20 +0300

    3rd task from for

--- Loops/for.py
+++ Loops/for.py
@@ -4,3 +4,12 @@ for i in range(10):
 a, b = input(), int(input())
 for i in range(b):
     print(a)
+
+for i in range(6):
+    print("AAA")
+for i in range(5):
+    print("BBBB")
+print("E")
+for i in range(9):
+    print("TTTTT")
+print("G")
