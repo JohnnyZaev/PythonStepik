dd7f02aa0c7a0d583f457d71cef52b59cd2a9a82
gvarys <johnnyzaev@gmail.com>
2022-09-12 13:40:59 +0300

    4th task from range overloads

--- Loops/for_range_overloads.py
+++ Loops/for_range_overloads.py
@@ -14,3 +14,8 @@ if (m % 2 == 0):
    m -= 1
 for i in range(m, n - 1, -2):
     print(i)
+
+m, n = int(input()), int(input())
+for i in range(m, n + 1):
+    if (i % 17 == 0 or i % 10 == 9 or i % 3 == 0 and i % 5 == 0):
+        print(i)
