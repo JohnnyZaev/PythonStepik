66e801cf817b9c0b33cedddaae16d035268584e7
gvarys <johnnyzaev@gmail.com>
2022-09-14 17:48:17 +0300

    2nd task from loops practice

--- Loops/for_practice.py
+++ Loops/for_practice.py
@@ -3,3 +3,8 @@ for i in range(int(input()), int(input()) + 1):
     if (i ** 3 % 10 == 4 or i ** 3 % 10 == 9):
         counter += 1
 print(counter)
+
+result = 0
+for i in range(int(input())):
+    result += int(input())
+print(result)
